d0c68750250f72b84e60c83da1b73dffa98f5e93
Eirik Sand <eiriksand@gmail.com>
2013-05-10 15:35:14 +0200

    Initial commit

--- src/main/clojure/watchdog/core.clj
+++ src/main/clojure/watchdog/core.clj
@@ -19,4 +19,41 @@
     (into-array WatchEvent$Kind (vals (select-keys event-kinds events)))))
 
 (defn watch [directory & events]
-  (-> (get-path directory) (.register watch-service (get-event-kinds events))))
+  (let [watch-key (-> (get-path directory) (.register watch-service (get-event-kinds events)))]
+    (let [directory-watcher-agent (agent watch-key)])
+    ))
+
+(defn start-watching []
+  (loop []
+    (let [key (.take watch-service)]
+      (prn (-> (.watchable key) (.getFileName)))
+      (if (= (.reset key) true) (recur)))))
+
+;  (while true
+;    (prn "Listenting...")
+;    (let [key (.take watch-service)]
+;      (prn (-> (.watchable key) (.getFileName)))
+;      (if-not (.reset key) (break))
+;      )))
+
+; Clojure agent example
+;(defn read-agent-error-handler [agnt, exception]
+;  (prn "Whoops! " agnt " had a problem: " exception))
+;
+;(def read-agent
+;  (agent
+;    "zero bytes"
+;    :validator string?
+;    :error-handler read-agent-error-handler))
+;
+;(defn big-read [old-value seconds]
+;  "Pretent to read a really big file"
+;  (time (Thread/sleep (* seconds 1000)))
+;  "<contents of big file>")
+;
+;(defn read-watch [key agnt old-value new-value]
+;  (prn "File has been read!")
+;  (prn (str "New file data is: " new-value))
+;  (prn ""))
+;
+;(add-watch read-agent "reader-01" read-watch)
